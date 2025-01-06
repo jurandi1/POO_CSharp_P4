133b38b50be4f746dd6d77e7527fbf97f208cd68
Jurandi Junior <jurandijr20@gmail.com>
2025-01-06 10:26:39 -0300

    Encapsulamento - Implementação manual

--- Produto.cs
+++ Produto.cs
@@ -4,52 +4,76 @@ namespace POO_CSharp_P4
 {
     internal class Produto
     {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         //Adicionando construtores
         //sobrecarga oferecendo mais de uma operação com o mesmo nome , porém com diferentes listas de
         //parâmetros.
         public Produto() {
-            Quantidade = 10;
+            _quantidade = 10;
         }
         //sobrecarga oferecendo mais de uma operação com o mesmo nome , porém com diferentes listas de
         //parâmetros.
         public Produto(string nome, double preco) : this()
         {
-            Nome = nome;
-            Preco = preco;
+            _nome = nome;
+            _preco = preco;
         }
         //sobrecarga oferecendo mais de uma operação com o mesmo nome , porém com diferentes listas de
         //parâmetros.
         public Produto(string nome, double preco, int quantidade) : this(nome, preco)
         {
-            Quantidade = quantidade;
+            _quantidade = quantidade;
+        }
+
+        //implementando métodos get e set(encapsulamento)
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            //Vantagens do encapsulamento aplicando lógica
+            if (nome != "" && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade() {
+            return _quantidade;
         }
 
         public double ValorTotalEmEstoque()
         {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade)
         {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProdutos(int quantidade)
         {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         public override string ToString()
         {
-            return Nome
+            return _nome
             + ", $ "
-            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
             + ", "
-            + Quantidade
+            + _quantidade
             + " unidades, Total: $ "
             + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
--- Program.cs
+++ Program.cs
@@ -7,45 +7,56 @@ namespace POO_CSharp_P4
     {
         private static void Main(string[] args)
         {
+
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.SetNome("TV 4K");
+
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+
+
+            //Equanto não há encapsulamento a inserção abaixa é permitida, o que não é o ideal. passe o atributo para private 
+            //p.Quantidade = -10;
             
-            Console.WriteLine("Entre os dados do produto:");
-            Console.Write("Nome: ");
-            string nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //Console.Write("Quantidade no estoque: ");
-            //int quantidade = int.Parse(Console.ReadLine());
-            //Adaptado com construtor
-            // Produto p = new Produto(nome, preco, quantidade);
+            //Console.WriteLine("Entre os dados do produto:");
+            //Console.Write("Nome: ");
+            //string nome = Console.ReadLine();
+            //Console.Write("Preço: ");
+            //double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ////Console.Write("Quantidade no estoque: ");
+            ////int quantidade = int.Parse(Console.ReadLine());
+            ////Adaptado com construtor
+            //// Produto p = new Produto(nome, preco, quantidade);
 
-            Produto p = new Produto(nome, preco);
+            //Produto p = new Produto(nome, preco);
 
-            Produto p2 = new Produto();
+            //Produto p2 = new Produto();
 
-            //Sintaxe alternativa para inicializar valores
-            Produto p3 = new Produto { 
-                Nome = "TV"
-                , Preco = 500.00
-                , Quantidade = 10
-            };
+            ////Sintaxe alternativa para inicializar valores
+            //Produto p3 = new Produto { 
+            //    Nome = "TV"
+            //    , Preco = 500.00
+            //    , Quantidade = 10
+            //};
 
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: " + p);
+            //Console.WriteLine();
+            //Console.WriteLine("Dados do produto: " + p);
 
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
+            //Console.WriteLine();
+            //Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            //int qte = int.Parse(Console.ReadLine());
+            //p.AdicionarProdutos(qte);
 
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser removido do estoque: ");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qte);
+            //Console.WriteLine();
+            //Console.WriteLine("Dados atualizados: " + p);
+            //Console.WriteLine();
+            //Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            //qte = int.Parse(Console.ReadLine());
+            //p.RemoverProdutos(qte);
 
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
+            //Console.WriteLine();
+            //Console.WriteLine("Dados atualizados: " + p);
             
 
         }
